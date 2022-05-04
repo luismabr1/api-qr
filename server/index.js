f84b394001e35b4d87582c918681d63c194c807c
Luis Manuel Brito <lsbrt4@gmail.com>
2022-05-04 11:15:51 -0400

    tratando de solucionar la seguridad

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const bodyParser = require('body-parser');
+// Import builtin NodeJS modules to instantiate the service
+const https = require("https");
+const fs = require("fs");
 const express = require('express');
 const app = express();
+
+
 const cors = require('cors');
 app.use(cors({ origin: true }));
 
@@ -14,6 +19,6 @@ app.use(bodyParser.json());
 app.use(require('./routes'));
 
 // Starting the server
-app.listen(app.get('port'), () => {
+https.createServer(app).listen(app.get('port'), () => {
   console.log(`Server on port ${app.get('port')}`);
 });
